4b93d58d438920d4bf77a12516ca35088fe9a8f5
koba0909 <hugh1mobile@gmail.com>
2023-03-09 01:39:06 +0900

    Container state 설정

--- app/src/main/java/com/example/playerscreentest/comtainer/ContainerViewModel.kt
+++ app/src/main/java/com/example/playerscreentest/comtainer/ContainerViewModel.kt
@@ -2,8 +2,43 @@ package com.example.playerscreentest.comtainer
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+data class ContainerState(
+    val isPortrait: Boolean,
+    val chatUiType: ChatUiType,
+)
+
+enum class ChatUiType {
+    NONE, RIGHT, BOTTOM_2, BOTTOM_4
+}
+
 @HiltViewModel
-class ContainerViewModel @Inject constructor(): ViewModel() {
+class ContainerViewModel @Inject constructor() : ViewModel() {
+    private val _state = MutableStateFlow(ContainerState(true, ChatUiType.NONE))
+    val state = _state.asStateFlow()
+
+    fun onToggleRotation() {
+        _state.update {
+            it.copy(
+                isPortrait = _state.value.isPortrait,
+            )
+        }
+    }
+
+    fun nextChatUiType() {
+        _state.update {
+            it.copy(
+                chatUiType = when (_state.value.chatUiType) {
+                    ChatUiType.RIGHT -> ChatUiType.BOTTOM_4
+                    ChatUiType.BOTTOM_4 -> ChatUiType.BOTTOM_2
+                    ChatUiType.BOTTOM_2 -> ChatUiType.NONE
+                    ChatUiType.NONE -> ChatUiType.RIGHT
+                }
+            )
+        }
+    }
 }
